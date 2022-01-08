c163f06c141fa1ebc5fc248072891a0b5a2d2d2e
Rugved Katole <f20180391@goa.bits-pilani.ac.in>
2022-01-08 13:32:07 +0530

    added walks and sumo wrapper

--- scripts/algorithms/Opttimal_patrolling_paper.py
+++ scripts/algorithms/Opttimal_patrolling_paper.py
@@ -70,7 +70,7 @@ def optimal_partition(graphML,m):
     dist = (i)*graphML.edges[g[i-1],g[i]]["length"]  #lenght of each edge is 100 which is added in chain graph. 
     wg.append(dist)
   #g=range(len(g))
-  g=list(map(int,g))
+  #g=list(map(int,g))
   wg=list(map(int,wg))
   a = 0
   delta = 100
@@ -94,12 +94,13 @@ def optimal_partition(graphML,m):
 
 class OPP:
 
-  def __init__(self, g, num_bots):
+  def __init__(self, g, num_bots,pis):
     self.graph=g
     self.num_bots = num_bots
+    self.pis=pis
     self.stamp = 0
     rospy.Service('algo_ready', AlgoReady, self.callback_ready)
-    self.ready = True
+    self.ready = False
 
 
   def callback_ready(self, req):
@@ -113,12 +114,18 @@ class OPP:
     node = req.node_done
     t = req.stamp
     bot = req.name
-    pis=optimal_partition(self.graph,self.num_bots)
-    node_list = pis[int(bot[-1])]
-    if int(node)+5<=24:
-      neigh = str(int(node)+5)
-    else:
-      neigh = str(int(node)-5)
+    node_list = pis[int(bot.split('_')[-1])]
+    '''add to and fro logic'''
+    direction=1
+    if direction==1:
+      if node==node_list[-1]:
+        direction=-1
+      neigh=node_list[node_list.index(node)+1]
+    elif direction==-1:
+      if node==node_list[-1]:
+        direction=-1
+      neigh=node_list[node_list.index(node)+1]
+    neigh=node_list[node_list.index(node)+1]
     next_walk = [node, neigh]
     next_departs = [t]
     return NextTaskBotResponse(next_departs, next_walk)
@@ -130,7 +137,11 @@ if __name__ == '__main__':
   graph_name = rospy.get_param('/graph')
   num_bots = int(rospy.get_param('/init_bots'))
   g = nx.read_graphml(dirname + '/graph_ml/' + graph_name + '.graphml')
+  pis = optimal_partition(g,num_bots)
+  rospy.set_param('init_locations',[p[0] for p in pis].join(' '))
+
   s=OPP(g,num_bots)
+
   #rospy.Subscriber('at_node', AtNode, s.callback_idle)
   rospy.Service('bot_next_task', NextTaskBot, s.callback_next_task)
 
