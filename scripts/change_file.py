968ed8a17c89ddf2c9ce5d255cf046eb6658cba1
Rugved Katole <f20180391@goa.bits-pilani.ac.in>
2022-02-02 10:59:57 +0530

    ...

---
+++ scripts/change_file.py
@@ -0,0 +1,11 @@
+import yaml
+
+for i in range(1,37):
+    with open('/home/workshop/mrpp_sumo/src/config/tpbp_alt1/tpbp_alt1_{}'.format(i)) as f:
+        list_doc = yaml.safe_load(f)
+
+    list_doc['depth'] = 3
+    list_doc['depth'] += '_{}'.format(list_doc['depth'])  
+
+    with open('/home/workshop/mrpp_sumo/src/config/tpbp_alt1_3/tpbp_alt1_{}_3'.format(i), "w") as f:
+        yaml.dump(list_doc, f)
