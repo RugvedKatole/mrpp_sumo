ca55b0d6e9591783e089df417cdfe7700cff2e61
Rugved Katole <f20180391@goa.bits-pilani.ac.in>
2022-02-11 10:33:45 +0530

    om

--- scripts/change_file.py
+++ scripts/change_file.py
@@ -15,9 +15,12 @@ def main(folder_name):
         # list_doc['depth'] = 3
         list_doc['algo_name'] = "{}_modified_{}".format(name_list[0],name_list[1])
         list_doc['random_string'] = '{}_{}_{}_d{}'.format(list_doc['algo_name'],list_doc['graph'],i,list_doc['depth'])
-        if not os.path.exists('{}/config/{}_{}'.format(dirname,list_doc['algo_name'],list_doc['depth'])):
-            os.mkdir('{}/config/{}_{}'.format(dirname,list_doc['algo_name'],list_doc['depth']))
-        with open('{}/config/{}_{}/{}_d{}_{}.yaml'.format(dirname,list_doc['algo_name'],list_doc['depth'],list_doc['algo_name'],list_doc['depth'],i), "w") as f:
+        # if not os.path.exists('{}/config/{}_{}'.format(dirname,list_doc['algo_name'],list_doc['depth'])):
+        if not os.path.exists('{}/config/{}'.format(dirname,list_doc['algo_name'])):
+            # os.mkdir('{}/config/{}_{}'.format(dirname,list_doc['algo_name'],list_doc['depth']))
+            os.mkdir('{}/config/{}'.format(dirname,list_doc['algo_name']))
+        # with open('{}/config/{}_{}/{}_d{}_{}.yaml'.format(dirname,list_doc['algo_name'],list_doc['depth'],list_doc['algo_name'],list_doc['depth'],i), "w") as f:
+        with open('{}/config/{}/{}_{}.yaml'.format(dirname,list_doc['algo_name'],list_doc['algo_name'],i), "w") as f:
             yaml.dump(list_doc, f)
 
 if __name__ == '__main__':
