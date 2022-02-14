b8d6605a9883f9e9bdf827631d3530c3bfe950ae
Rugved Katole <f20180391@goa.bits-pilani.ac.in>
2022-02-14 11:55:29 +0530

    ....

--- scripts/post_process/combine_plots.py
+++ scripts/post_process/combine_plots.py
@@ -31,14 +31,15 @@ def main(param):
     # for i in range(1,12,3):
     # algos=['tpbp_final','tpbp_alt1_1','tpbp_util','tpbp_util1_1']
     # algos = ['tpbp_util1_1','tpbp_util1_3','tpbp_util1_5','tpbp_util']
-    algos = ['through_basic_1','through_FHUM_1','through_basic_3']
-    # algos = ['through_FHUM_3','through_basic_5','through_FHUM_5']
+    algos = ['through_modified_basic_1','through_modified_basic_3','through_modified_basic_5']
+    # algos = ['through_modified_FHUM_3','through_modified_FHUM_3','through_modified_FHUM_5']
+    colors =['red','blue','green']
     fig,axes = plt.subplots(3,4,figsize=(30,15))
     i=0
     for j in range(4):
         for k in range(3):
             i += 1
-            for a in algos:
+            for z,a in enumerate(algos):
                 if len(a.split("_"))==3:
                     with open('{}/config/{}/{}_{}_{}.yaml'.format(dirname, a, "_".join(a.split('_')[:-1]), str(i),a.split('_')[-1]), 'r') as f:
                         config = yaml.load(f, yaml.FullLoader)
@@ -54,20 +55,22 @@ def main(param):
                 priority_nodes = config['priority_nodes'].split(' ')
                 non_priority_nodes = [u for u in graph.nodes if u not in priority_nodes]
                 time_period = config['time_periods'].split(' ')
-                sns.lineplot(ax=axes[k,j],data = df1.iloc[::1000], x = list(range(0,len(df1.loc[::1000])*1000,1000)), y = df1.loc[::1000, nodes].mean(axis = 1), legend = True, linewidth = 3,alpha=1)
+                for n in priority_nodes:
+                    sns.scatterplot(ax=axes[k,j],data= df1.loc[::1000,n], color = colors[z], alpha = 1)
+                sns.lineplot(ax=axes[k,j],data = df1.iloc[::1000], x = list(range(0,len(df1.loc[::1000])*1000,1000)), y = df1.loc[::1000, priority_nodes].mean(axis = 1), legend = True, linewidth = 3,alpha=1,color = colors[z])
                 # sns.lineplot(ax=axes[k,j],data = df1.iloc[::1000], x = list(range(0,len(df1.loc[::1000])*1000,1000)), y = df1.loc[::1000, priority_nodes].mean(axis = 1), legend = True, linewidth = 3)
             axes[k,j].set_title("{} robots and {} priority nodes".format(config['init_bots'],k+4))
-            axes[k,j].set_ylim(0,400)
+            axes[k,j].set_ylim(0,500)
             # axes[k,j].set_xlim(0)
             sns.lineplot(ax=axes[k,j],data = df1.iloc[::1000], x = list(range(0,len(df1.loc[::1000])*1000,1000)), y = float(time_period[0]), legend = True, linewidth = 3,alpha=0.4)
-            # algo_name=['tpbp_basic','tpbp_FHUM','depth_basic','depth_FHUM','through_basic','through_FHUM']
+            # algo_name=['tpbp_modified_basic','tpbp_modified_FHUM','depth_modified_basic','depth_modified_FHUM','through_modified_basic','through_modified_FHUM']
             axes[k,j].legend(algos,prop={'size': 9})
             # axes[k,j].set_xlabel('time in seconds')
             plt.xticks(rotation = 30)
             # plt.ylabel('Node Idleness')
             # plt.xlabel("time in seconds")
-    # plt.suptitle('Node Idlesness vs Time, through algo all depths', size = 40, y = 1.02, x = 0.4)
-    plt.savefig('{}/depths_FHUM_{}.png'.format(dirname, str(i/12)), bbox_inches='tight')
+    plt.suptitle('Node Idlesness vs Time, through modified basic with 1,3,5  depths', size = 40, y = 1.02, x = 0.4)
+    plt.savefig('{}/through modified basic with 1,3,5  depths_{}.png'.format(dirname, str(i/12)), bbox_inches='tight')
 
 
 
